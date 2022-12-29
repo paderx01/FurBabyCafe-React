c7b0bb3e1e17bd28386c22d3c1c8b0f61d9f7139
Pader Xiong <paderx26@gmail.com>
2022-12-28 18:21:41 -0600

    imported files from json package to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-social/bootstrap-social.css";
+import "font-awesome/css/font-awesome.css";
+import "typeface-lobster";
+import "typeface-open-sans";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
