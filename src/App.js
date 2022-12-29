1abb09f098343eb47a0f7e1e57ab36c4c6f673f4
Pader Xiong <paderx26@gmail.com>
2022-12-28 18:16:31 -0600

    added dependencies to json package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
 
 function App() {
-  return null
-
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
