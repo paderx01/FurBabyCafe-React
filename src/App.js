b94645fa4d860af8407e5b1176a13c7a661712af
Pader Xiong <paderx26@gmail.com>
2022-12-30 03:25:23 -0600

    Added aboutus component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Container, Navbar, NavbarBrand } from "reactstrap";
 import logo from "./app/assets/img/main.png";
+import AboutPage from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="light" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <img
-              style={{
-                width: 700,
-                height: 350,
-                alignItems: "center",
-              }}
-              src={logo}
-              alt="Main Page"
-            />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
+      <Container>
+        <img
+          style={{
+            width: 700,
+            height: 350,
+            alignItems: "center",
+          }}
+          src={logo}
+          alt="Main Page"
+        />
+      </Container>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path=".aboutus" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
